f0fe45c868cc2a066aacd7e976b0415ec58606bf
vwtorr <v.gabrielsilva2010@gmail.com>
2025-08-11 21:08:35 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,26 @@ import Image from "next/image";
 import { Header } from "@/components/ui/common/header";
 import { db } from "@/db";
 import ProductList from "@/components/ui/common/product-list";
+import { productTable } from "@/db/schema";
+import { desc } from "drizzle-orm";
+import CategorySelector from "@/components/ui/common/category-selector";
+import Footer from "@/components/ui/common/footer";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
     with: {
-    variants: true,
-  }
+      variants: true,
+    },
   });
-  
+
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants: true,
+    },
+  });
+  const categories = await db.query.categoryTable.findMany({});
+
   return (
     <>
       <Header />
@@ -20,10 +32,14 @@ const Home = async () => {
           height={0}
           width={0}
           sizes="100vm"
-          className="h-auto w-full"
+          className="h-auto w-full rounded-3xl"
         ></Image>
 
-        <ProductList products={products} title="Mais Vendidos"/>
+        <ProductList products={products} title="Mais Vendidos" />
+
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
 
         <Image
           src="/banner02.png"
@@ -31,9 +47,12 @@ const Home = async () => {
           height={0}
           width={0}
           sizes="100vm"
-          className="h-auto w-full"
+          className="rounded-4xl h-auto w-full"
         ></Image>
+
+        <ProductList products={newlyCreatedProducts} title="Novidades" />
       </div>
+      <Footer />
     </>
   );
 };
