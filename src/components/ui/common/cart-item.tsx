fb741c685b1fbbdced91e45525d927a6deabb898
vwtorr <v.gabrielsilva2010@gmail.com>
2025-08-15 09:07:20 -0300

    feat: ensure product can be removed from cart

--- src/components/ui/common/cart-item.tsx
+++ src/components/ui/common/cart-item.tsx
@@ -4,6 +4,13 @@ import Image from "next/image";
 import { formatCentsToBRL } from "@/helpers/money";
 import { Button } from "../button";
 import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
+import {
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { removeProductFromCart } from "@/actions/remove-cart-product";
+import { toast } from "sonner";
 
 interface CartItemProps {
   id: string;
@@ -22,6 +29,27 @@ const CartItem = ({
   productVariantPriceInCents,
   quantity,
 }: CartItemProps) => {
+  const queryClient = useQueryClient();
+
+const removeProductFromCartMutation = useMutation({
+  mutationKey: ["remove-cart-product"],
+ mutationFn: () => removeProductFromCart({ cartItemId: id }),
+  onSuccess: () => {
+    queryClient.invalidateQueries({ queryKey: ["cart"] });
+  },
+});
+
+const handleDeleteClick = () => {
+  removeProductFromCartMutation.mutate(undefined, {
+    onSuccess: () => {
+      toast.success("Produto removido do carrinho.");
+    },
+    onError: () => {
+      toast.error("Erro ao remover produto do carrinho.");
+    },
+  });
+};
+
   return (
     <div
       key={id}
@@ -54,11 +82,11 @@ const CartItem = ({
         </div>
       </div>
       <div className="flex flex-col items-end justify-center gap-2">
-        <Button variant="outline" size="icon">
-          <TrashIcon></TrashIcon>
+        <Button variant="outline" size="icon" onClick={handleDeleteClick}>
+          <TrashIcon />
         </Button>
 
-        <p className="font-semibold">
+        <p className="font-sans">
           {formatCentsToBRL(productVariantPriceInCents * quantity)}
         </p>
       </div>
--- src/components/ui/common/cart.tsx
+++ src/components/ui/common/cart.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { ShoppingBasketIcon } from "lucide-react";
 import {
   Sheet,
@@ -9,12 +8,13 @@ import {
   SheetTitle,
 } from "../sheet";
 import { Button } from "../button";
+import { Separator } from "../separator";
 import { useQuery } from "@tanstack/react-query";
 import { getCart } from "@/actions/get-cart";
 import { formatCentsToBRL } from "@/helpers/money";
 import CartItem from "./cart-item";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
-import { Separator } from "@radix-ui/react-separator";
+import Link from "next/link";
 
 const Cart = () => {
   const { data, isLoading, isError } = useQuery({
@@ -41,13 +41,12 @@ const Cart = () => {
         <SheetHeader>
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
-
         <div className="flex h-full flex-col px-5 pb-5">
           <div className="flex h-full max-h-full flex-col overflow-hidden">
-            <ScrollArea className="h-full">
-              <div className="flex h-full flex-col gap-8">
-                {data?.items?.length ? (
-                  <>
+            <div className="flex flex-col h-full">
+              <ScrollArea className="flex-1">
+                <div className="flex flex-col gap-4">
+                  {data?.items?.length ? (
                     <ul className="space-y-4">
                       {data.items.map((item) => (
                         <li key={item.id}>
@@ -55,44 +54,50 @@ const Cart = () => {
                             id={item.id}
                             productName={item.productVariant.product.name}
                             productVariantName={item.productVariant.name}
-                            productVariantImageUrl={item.productVariant.imageUrl}
-                            productVariantPriceInCents={item.productVariant.priceInCents}
+                            productVariantImageUrl={
+                              item.productVariant.imageUrl
+                            }
+                            productVariantPriceInCents={
+                              item.productVariant.priceInCents
+                            }
                             quantity={item.quantity}
                           />
                         </li>
                       ))}
                     </ul>
-
-                    <div className="flex flex-col gap-4">
-                      <Separator />
-                      <div className="flex items-center justify-between text-xs font-medium">
-                        <p>Subtotal</p>
-                        <p>{formatCentsToBRL(data.totalPriceInCents ?? 0)}</p>
-                      </div>
-
-                      <Separator/>
-
-                       <div className="flex items-center justify-between text-xs font-medium">
-                        <p>Entrega</p>
-                        <p>GRÁTIS</p>
-                      </div>
-
-                      <Separator/>
-
-                       <div className="flex items-center justify-between text-xs font-medium">
-                        <p>Total</p>
-                        <p>GRÁTIS</p>
-
-                        <Button className="mt-5 rounted-full">Finalizar compra</Button>
-                      </div>
-
-                    </div>
-                  </>
-                ) : (
-                  <p className="text-gray-500">Seu carrinho está vazio</p>
-                )}
-              </div>
-            </ScrollArea>
+                  ) : (
+                    <p className="text-gray-500">Seu carrinho está vazio</p>
+                  )}
+                </div>
+              </ScrollArea>
+              
+              {data?.items?.length ? (
+                <div className="mt-auto pt-4 space-y-3">
+                  <div className="flex items-center justify-between text-xs font-medium">
+                    <p>Subtotal</p>
+                    <p>{formatCentsToBRL(data?.totalPriceInCents ?? 0)}</p>
+                  </div>
+                  
+                  <Separator />
+                  
+                  <div className="flex items-center justify-between text-xs font-medium">
+                    <p>Entrega</p>
+                    <p>GRÁTIS</p>
+                  </div>
+                  
+                  <Separator />
+                  
+                  <div className="flex items-center justify-between text-xs font-medium">
+                    <p>Total</p>
+                    <p>{formatCentsToBRL(data?.totalPriceInCents ?? 0)}</p>
+                  </div>
+                  
+                  <Button className="mt-5 rounded-full w-full" asChild>
+                    <Link href="/cart/identification">Finalizar compra</Link>
+                  </Button>
+                </div>
+              ) : null}
+            </div>
           </div>
         </div>
       </SheetContent>
